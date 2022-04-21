f39f50be05bf6df3ee5f679ed511c5d16e6c498a
MCprotein <chss3339@gmail.com>
2022-04-22 01:01:50 +0900

    MVC의 Controller 분리하기

--- app.js
+++ app.js
@@ -1,56 +1,23 @@
+"use strict";
+
+// 모듈
 const express = require('express');
 const app = express();
+// html로 작성하여 화면에 띄우려면 라우터나 컨트롤러 쪽에서 html을 읽은 후에 바디에 실어서 클라이언트에게 넘겨줘야 하는 번거로운 작업이 필요하지만, ejs로 하면 바로 렌더링 할 수 있다.
+// https://jogakjungbo.tistory.com/27 폴더컴팩트
 
-app.get('/', (req, res) => {
-    // 기능
-    res.send(`
-    <!DOCTYPE html>
-    <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    </head>
-    <body>
-        여기는 루트입니다.
-    </body>
-    </html>`
-    );
-});
+const PORT = 3000;
+// 라우팅
+const home = require('./routes/home');
 
-app.get('/login', (req, res) => {
-    res.send(`
-    <!DOCTYPE html>
-    <html lang="ko">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    </head>
-    <body>
-        <input type="text" placeholder="아이디"><br>
-        <input type="text" placeholder="비밀번호"><br>
-        <button>로그인</button>
-    </body>
-    </html>`
-    );
-});
-app.listen(3000, () => {
+// 앱 세팅
+app.set('views', './views'); // views 경로지정
+app.set('view engine', 'ejs');
+
+
+app.use("/", home); // user -> 미들웨어를 등록해주는 메서드
+
+app.listen(PORT, () => {
     console.log("서버 기동");
 });
 
-// const http = require('http');
-// const app = http.createServer((req, res) => {
-//     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-//     if (req.url === '/') {
-//         res.end('여기는 루트입니다.');
-//     } else if (req.url === '/login') {
-//         res.end('여기는 로그인 화면입니다.');
-//     }
-// });
-
-// app.listen(3001, () => {
-//     console.log('http로 가동된 서버입니다.');
-// });
